f90fd70c7da61fbe31b1cd1027fee284a28d5f15
Azgilord <battlefury36@gmail.com>
2022-12-15 00:43:07 +0300

    второе задание

---
+++ by/itacademy/hw2/Task2.java
@@ -0,0 +1,13 @@
+package by.itacademy.hw2;
+
+public class Task2{
+    public static void main(String[] args) {
+        System.out.println( result (5, 6) );
+
+    }
+        static int result(int a, int b) {
+        return (a + b)+ a*b;
+    }
+    }
+
+
