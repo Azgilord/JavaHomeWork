f7eeb382801d1b4df73ad09e13341c1e314a9a9d
Azgilord <battlefury36@gmail.com>
2023-01-14 16:03:56 +0300

    5 ДЗ

---
+++ by/itacademy/hw5/Task1/Main.java
@@ -0,0 +1,11 @@
+package by.itacademy.hw5.Task1;
+
+public class Main {
+    public static void main(String[] age) {
+        Person ivan = new Person("Ivan", 25);
+        Person leha = new Person();
+        ivan.talk();
+        leha.move();
+
+    }
+}
